f1ce491e0a3f1b3dd7d1b34e1b18f113677310eb
I-am-me <I-am-me@users.noreply.github.com>
2021-05-15 11:11:46 +0200

    BLTouch on Z_Min

--- Configuration.h
+++ Configuration.h
@@ -687,7 +687,7 @@
  *
  * Enable this option for a probe connected to the Z Min endstop pin.
  */
-//#define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN
+#define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN
 
 /**
  * Z_MIN_PROBE_ENDSTOP
@@ -708,7 +708,8 @@
  * disastrous consequences. Use with caution and do your homework.
  *
  */
-#define Z_MIN_PROBE_ENDSTOP
+//#define Z_MIN_PROBE_ENDSTOP
+//#define Z_MIN_PROBE_PIN 32  //Z-MAX endstop pin
 
 /**
  * Probe Type
